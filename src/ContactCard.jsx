929c44f08102b6127e72b179ccfa48ee737bd2a3
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-06-13 23:06:43 -0400

    Added ContactCard component

---
+++ src/ContactCard.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class ContactCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        hidden: true
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
